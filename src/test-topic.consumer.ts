047b1c94fb495928d66bfd5900e5d276fed756ef
ghislaingenay <ghislain.gny@gmail.com>
2023-01-23 18:17:55 +0700

    Refactor testConsumer

--- src/test-topic.consumer.ts
+++ src/test-topic.consumer.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConsumerService } from './kafka/consumer.service';
 
+// This is the consumer for the test-topic topic name
 @Injectable()
 export class TestTopicConsumer implements OnModuleInit {
   constructor(private readonly consumerService: ConsumerService) {}
@@ -9,17 +10,14 @@ export class TestTopicConsumer implements OnModuleInit {
   }
 
   async onModuleInit() {
-    await this.consumerService.consume(
-      { topics: ['test-topic'] },
-      {
-        eachMessage: async ({ topic, partition, message }) => {
-          console.log({
-            value: message.value.toString(),
-            topic: topic.toString(),
-            partition: partition.toString(),
-          });
-        },
+    // all consumers inside a consumer group with the same group Id will be consuming from a given topic amoongst n number of partitions => partitions will be distributed to all memebers inside this particular group
+    await this.consumerService.consume({
+      topic: { topics: ['test-topic'], fromBeginning: true },
+      config: { groupId: 'test-consumer' },
+      onMessage: async (message) => {
+        console.log(message.value.toString());
       },
-    ); // eachMessage: code run when a message is received in this topic
+    });
+    // eachMessage: code run when a message is received in this topic
   }
 }
